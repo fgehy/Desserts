c42118bebe619991931dad0e858e335d6bb3d057
Fabrice Gehy <fabricegehy@gmail.com>
2024-08-28 12:05:00 -0400

    add project

---
+++ DessertApp/DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Fabrice Gehy on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
